ed80870c3a038af53d7deebe1c4fb44a08f4503e
tmhoang018 <110443186+tmhoang018@users.noreply.github.com>
2023-08-06 14:42:36 +0700

    Add files via upload

---
+++ database_esp.ino
@@ -0,0 +1,78 @@
+/*
+ * HTTP Client POST Request
+ * Copyright (c) 2018, circuits4you.com
+ * All rights reserved.
+ * https://circuits4you.com 
+ * Connects to WiFi HotSpot. */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "B11";  //ENTER YOUR WIFI SETTINGS
+const char *password = "hoangkhanhute";//Riclabspkt@123
+
+//Web/Server address to read/write from 
+const char *host = "192.168.1.3:80";   //https://circuits4you.com website or IP address of server
+//172.18.10.106
+//=======================================================================
+//                    Power on setup
+//=======================================================================
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  Serial.print("Connecting");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+}
+
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
+void loop() {
+  HTTPClient http;    //Declare object of class HTTPClient
+
+  String ADCData, station, postData;
+  int adcvalue=analogRead(A0);  //Read Analog value of LDR
+  //ADCData = String(adcvalue);   //String to interger conversion
+  //station = "A";
+
+  String Name = "Tran Minh Hoang";
+  int Timesleep = random(0,10);
+  int Yawn = random(0,5);
+  //Post Data
+  postData = "name=" + Name + "& timesleep=" + Timesleep + "& yawn=" + Yawn ;
+  asdfasdfs + Hoang sdkfhaskdfh + 30 eruweriyt + 10
+  http.begin("http://192.168.1.3/system/postdemo.php");              //Specify request destination
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
+
+  int httpCode = http.POST(postData);   //Send the request
+  String payload = http.getString();    //Get the response payload
+
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+
+  http.end();  //Close connection
+  
+  delay(5000);  //Post Data at every 5 seconds
+}
